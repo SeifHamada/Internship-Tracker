c1abd1967757b95cc50dc26bd87f15b97649c063
Mohamed Ramzy <mohamedramzy2006@gmail.com>
2026-04-04 19:08:27 +0200

    chore(main): disable debug banner and integrate global navigation shell
    - Remove debug banner
    - Update root app to launch into centralized HomeScreen

--- frontend/internshiptracker/lib/main.dart
+++ frontend/internshiptracker/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'screens/dashboard_screen.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const InternshipTrackerApp());
@@ -13,11 +13,12 @@ class InternshipTrackerApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Internship Tracker',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1565C0)),
         useMaterial3: true,
       ),
-      home: const DashboardScreen(),
+      home: const HomeScreen(),
     );
   }
 }
